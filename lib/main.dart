82d5bd26f9af92d79c7639bc5afac8cbaec9109f
Tanawoot88 <87426334+Tanawoot88@users.noreply.github.com>
2021-10-09 21:04:15 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:test/game_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:test/food/home_page.dart';
+import 'package:test/food/login_page.dart';
+
+
+import 'food/food_details.dart';
 
 
 void main() {
@@ -13,21 +18,24 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch:Colors.blueGrey,
+        fontFamily: GoogleFonts.prompt().fontFamily,
+        primarySwatch: Colors.blueGrey,
+        textTheme: const TextTheme(
+          headline1: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
+          bodyText1: TextStyle(fontSize: 30.0),
+          bodyText2: TextStyle(fontSize: 18.0),
+        ),
       ),
-      home: GamePage(),
+      initialRoute: '/login',
+      routes: {
+        LoginPage.routeName : (context) => LoginPage(),
+        HomePage.routeName : (context) => HomePage(),
+        FoodDetails.routeName :(context) => FoodDetails(),
+      },
     );
   }
-}
+  }
+
 
 
 
